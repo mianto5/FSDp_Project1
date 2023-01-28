d0692061eef5b6d3e6b8629fa9be13eec5c3100a
mianto5 <tomanovamichaela5@gmail.com>
2023-01-28 18:08:08 +0100

    FSD program: Project 1, Sprint 2 - defines "Files" class

--- src/FSDp_Project1/Files.java
+++ src/FSDp_Project1/Files.java
@@ -1,4 +1,25 @@
 package FSDp_Project1;
 
-public class Files {
+public class Files implements Comparable<Files>{
+
+    private String fName;
+
+    public Files(String fName) {
+        this.fName = fName;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    @Override
+    public String toString() {
+        return fName;
+    }
+
+    @Override
+    public int compareTo(Files file) {
+        return getfName().toLowerCase().compareTo(file.getfName().toLowerCase());
+    }
+
 }
